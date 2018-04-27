551ed56ad43e3a4b945a74eb4294557877a1c6da
heineiuo <heineiuo@gmail.com>
2018-04-27 09:45:56 +0800

    Update type definition

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@
 // Fix for https://github.com/heineiuo/isomorphic-ws/issues/8
 // If there is still something wrong, welcome issue.
 
-import WebSocket from 'ws'
+import WebSocket = require('ws')
 
-export default WebSocket
+export = WebSocket
