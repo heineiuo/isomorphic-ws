c8943bda8da38d9f3501cbe522fdd6aa714b731e
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-07-09 11:33:19 +0200

    Added rollup example

---
+++ example/rollup/app.js
@@ -0,0 +1,4 @@
+import WebSocket from 'isomorphic-ws'
+
+
+const ws = new WebSocket('ws://www.host.com/path')
---
+++ example/rollup/rollup.config.js
@@ -0,0 +1,11 @@
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "app.js",
+  output: {
+    sourcemap: false,
+    format: "es",
+    file: "app.output.js",
+  },
+  plugins: [resolve()],
+};
