b255b999fc63a94af561b09ea7e877c3da7051c2
heineiuo <heineiuo@gmail.com>
2022-06-27 18:44:00 +0800

    Revert "Update index.d.ts"
    This reverts commit b21062c9bda56410acb82606d508f89f2980ef5d.

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@
 // Fix for https://github.com/heineiuo/isomorphic-ws/issues/8
 // If there is still something wrong, welcome issue.
 
-import WebSocket = require('ws');
+import WebSocket = require('ws')
 
-export default WebSocket;
+export = WebSocket
